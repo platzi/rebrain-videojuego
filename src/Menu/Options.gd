ab64a423c94fc417ccdf73b5aa7fb836d23de0f8
qwe <jeduardoochavez@gmail.com>
2022-05-25 04:09:41 -0600

    Se modificaron los nombres de variables para seguir el estandar de godot

--- src/Menu/Options.gd
+++ src/Menu/Options.gd
@@ -1,17 +1,20 @@
 extends Control
 
-signal close_options
 
-onready var backBtn := $BackBtn
+signal close_options
+onready var back_btn := $BackBtn
+
 
 func _on_BackBtn_pressed():
 	emit_signal("close_options")
 	queue_free()
 
+
 func _input(event):
 	if event is InputEventKey && event.scancode == KEY_ESCAPE:
 		_on_BackBtn_pressed()
 
+
 func _ready():
-	backBtn.connect("pressed", self, "_on_BackBtn_pressed")
-	backBtn.grab_focus()
+	back_btn.connect("pressed", self, "_on_BackBtn_pressed")
+	back_btn.grab_focus()
