943e2ba610a9f40b9307c0ae9e066a3fbef4605f
qwe <jeduardoochavez@gmail.com>
2022-05-26 01:34:12 -0600

    Update animations

--- src/Entity/Worm.gd
+++ src/Entity/Worm.gd
@@ -2,10 +2,15 @@ extends Entity
 
 
 func _ready():
-	speed = 40
+	speed = 80
 
 func _input(event : InputEvent) -> void:
 	if event is InputEventKey:
 		moving = true
+		if event.scancode == KEY_ESCAPE and event.pressed:
+			turns_towards("left")
+		elif event.scancode == KEY_SPACE and event.pressed:
+			turns_towards("right")
 	else:
 		moving = false
+	
