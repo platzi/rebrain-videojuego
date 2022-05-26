6a6a238734edecddc12ab10c551bf1ea8ee5f6ab
qwe <jeduardoochavez@gmail.com>
2022-05-25 19:14:19 -0600

    Added variables and funcions types, and changed the naming convention of two instances into snake case

--- src/Menu/Options.gd
+++ src/Menu/Options.gd
@@ -5,16 +5,16 @@ signal close_options
 onready var back_btn := $BackBtn
 
 
-func _on_BackBtn_pressed():
+func _on_BackBtn_pressed() -> void:
 	emit_signal("close_options")
 	queue_free()
 
 
-func _input(event):
+func _input(event : InputEvent) -> void:
 	if event is InputEventKey && event.scancode == KEY_ESCAPE:
 		_on_BackBtn_pressed()
 
 
-func _ready():
+func _ready() -> void:
 	back_btn.connect("pressed", self, "_on_BackBtn_pressed")
 	back_btn.grab_focus()
