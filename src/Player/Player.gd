4d78261898956fc3e853ee6fab7e97b12f99f3d0
qwe <jeduardoochavez@gmail.com>
2022-05-26 00:07:33 -0600

    Update the collisions of the player and delete redudndant code

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -4,13 +4,12 @@ extends KinematicBody2D
 const MAX_SPEED = 200
 var input_vector = Vector2.ZERO
 var velocity = Vector2.ZERO
-onready var animation_player = $AnimationPlayer
 
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
 
-func _physics_process(_delta):
+func _physics_process(_delta : float) -> void:
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
@@ -20,10 +19,7 @@ func _physics_process(_delta):
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Move/blend_position", input_vector)
 		animation_state.travel("Move")
-		print(velocity)
+		velocity = move_and_slide(velocity)
 	else:
 		animation_state.travel("Idle")
-		velocity = Vector2.ZERO
-	
-	velocity = move_and_slide(velocity)
-	
+
