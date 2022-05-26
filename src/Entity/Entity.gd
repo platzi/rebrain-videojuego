f812f2cea4a26130b80244fbfb1ad3b7290f2c68
qwe <jeduardoochavez@gmail.com>
2022-05-26 01:33:12 -0600

    Update methods

--- src/Entity/Entity.gd
+++ src/Entity/Entity.gd
@@ -9,7 +9,7 @@ onready var move_vector = animation_tree.get("parameters/Idle/blend_position")
 onready var animation_state = animation_tree.get("parameters/playback")
 var moving = false
 var speed = 100
-
+var velocity_vector = Vector2.ZERO
 
 func turns_towards(towards : String) -> void:
 	if towards.to_lower() == "left":
@@ -17,28 +17,25 @@ func turns_towards(towards : String) -> void:
 		#(-1, -1) -- (-1, 1) -- (1, 1) -- (1, -1)
 		if move_vector.x > 0:
 			move_vector += Vector2(-1, -1)
-		else:
+		elif move_vector.x < 0:
 			move_vector += Vector2(1, 1)
-		
-		if move_vector.y > 0:
+		elif move_vector.y > 0:
 			move_vector += Vector2(1, -1)
-		else:
+		elif move_vector.y < 0:
 			move_vector += Vector2(-1, 1)
 	elif towards.to_lower() == "right":
 		#(1, 0) -> (0, 1) -> (-1, 0) -> (0, -1)
 		#(-1, 1) -- (-1, -1) -- (1, -1) -- (1, 1)
 		if move_vector.x > 0:
 			move_vector += Vector2(-1, 1)
-		else:
+		elif move_vector.x < 0:
 			move_vector += Vector2(1, -1)
-		
-		if move_vector.y > 0:
+		elif move_vector.y > 0:
 			move_vector += Vector2(-1, -1)
-		else:
+		elif move_vector.y < 0:
 			move_vector += Vector2(1, 1)
 	animation_tree.set("parameters/Idle/blend_position", move_vector)
 	
-
 func move_forward() -> void:
 	moving = true
 
@@ -49,8 +46,9 @@ func stop_moving() -> void:
 
 func _physics_process(delta : float) -> void:
 	if moving:
-		animation_tree.set("parameters/Move/blend_position", move_vector)
+		animation_tree.set("parameters/Move/BlendSpace2D/blend_position", move_vector)
 		animation_state.travel("Move")
-		move_and_slide(move_vector * speed)
+		velocity_vector = move_vector * speed
+		velocity_vector = move_and_slide(velocity_vector)
 	else:
 		animation_state.travel("Idle")
