c655ded08f65fce6cbd52ffd87093b816c72aba0
qwe <jeduardoochavez@gmail.com>
2022-05-26 19:26:09 -0600

    Update functions for the messages

--- src/Entity/Entity.gd
+++ src/Entity/Entity.gd
@@ -7,6 +7,7 @@ class_name Entity
 onready var animation_tree = $AnimationTree
 onready var move_vector = animation_tree.get("parameters/Idle/blend_position")
 onready var animation_state = animation_tree.get("parameters/playback")
+onready var message_board = $MessageBoard
 var moving = false
 var speed = 100
 var velocity_vector = Vector2.ZERO
@@ -43,12 +44,33 @@ func move_forward() -> void:
 func stop_moving() -> void:
 	moving = false
 
+func show_message() -> void:
+	message_board.show_message()
+
+
+func hide_message() -> void:
+	message_board.hide_message()
+
+
+func set_text(message : String) -> void:
+	message_board.set_text(message)
+
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		if event.scancode == KEY_ENTER:
+			show_message()
+		elif event.scancode == KEY_CONTROL:
+			hide_message()
+		elif event.scancode == KEY_0:
+			set_text("ASDFGHJKAKKSAJDJAD")
+
 
 func _physics_process(delta : float) -> void:
 	if moving:
 		animation_tree.set("parameters/Move/BlendSpace2D/blend_position", move_vector)
 		animation_state.travel("Move")
 		velocity_vector = move_vector * speed
-		velocity_vector = move_and_slide(velocity_vector)
+		velocity_vector = move_and_slide(velocity_vector) 
 	else:
 		animation_state.travel("Idle")
