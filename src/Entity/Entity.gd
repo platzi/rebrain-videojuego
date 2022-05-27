f47dbb6425828ff9787f640cd6611ae28ff008ca
eduardochavez64 <jeduardoochavez@gmail.com>
2022-05-27 02:48:42 -0600

    Added continous shooting with a delay and stop shooting, and remove bullet when going beyond the screen

--- src/Entity/Entity.gd
+++ src/Entity/Entity.gd
@@ -5,12 +5,17 @@ extends KinematicBody2D
 
 
 onready var animation_tree = $AnimationTree
+onready var message_board = $MessageBoard
 onready var move_vector = animation_tree.get("parameters/Idle/blend_position")
 onready var animation_state = animation_tree.get("parameters/playback")
-onready var message_board = $MessageBoard
+onready var velocity_vector = animation_tree.get("parameters/Idle/blend_position")
+
 var moving = false
 var speed = 100
-onready var velocity_vector = animation_tree.get("parameters/Idle/blend_position")
+var cool_down = 1
+var _timer = null
+var is_shooting = false
+
 
 func turns_towards(towards : String) -> void:
 	if towards.to_lower() == "left":
@@ -64,6 +69,23 @@ func shoot() -> void:
 	bullet.shoot(move_vector)
 
 
+func start_shooting() -> void:
+	shoot()
+	_timer = Timer.new()
+	add_child(_timer)
+	_timer.connect("timeout", self, "shoot")
+	_timer.set_wait_time(cool_down)
+	_timer.set_one_shot(false)
+	_timer.start()
+	is_shooting = true
+
+
+func stop_shooting() -> void:
+	_timer.queue_free()
+	_timer = null
+	is_shooting = false
+
+
 func _input(event) -> void:
 	if event is InputEventKey and event.is_pressed():
 		if event.scancode == KEY_ENTER:
@@ -72,8 +94,10 @@ func _input(event) -> void:
 			hide_message()
 		elif event.scancode == KEY_0:
 			set_text("ASDFGHJKAKKSAJDJAD")
-		elif event.scancode == KEY_1:
-			shoot()
+		elif event.scancode == KEY_1 and not is_shooting:
+			start_shooting()
+		elif event.scancode == KEY_2 and is_shooting:
+			stop_shooting()
 
 func _physics_process(delta : float) -> void:
 	if moving:
