3a27b3fdc7449c23b7500a6db20360ddee003066
arnoldochavez <arnoldochavezlopez@hotmail.com>
2022-05-27 17:53:26 -0600

    Removed default brain values

--- src/Scripting/Brain.gd
+++ src/Scripting/Brain.gd
@@ -8,7 +8,7 @@ signal shoot
 signal show_message
 signal hide_message
 
-var brain := {"RotateNode":{"type":"ROTATE","position":[220,40],"connections":[{"from_port":0,"to":"MoveForwardNode","to_port":0}],"params":[1]},"UpdateNode":{"type":"UPDATE","position":[20,60],"connections":[{"from_port":0,"to":"RotateNode","to_port":0}],"params":[]},"MoveForwardNode":{"type":"MOVE_FORWARD","position":[420,40],"connections":[{"from_port":0,"to":"TimerNode","to_port":0}],"params":[2]},"TimerNode":{"type":"TIMER","position":[620,40],"connections":[{"from_port":0,"to":"ShootNode","to_port":0}],"params":[5]},"ShootNode":{"type":"SHOOT","position":[820,60],"connections":[{"from_port":0,"to":"MessageNode","to_port":0}],"params":[]},"MessageNode":{"type":"MESSAGE","position":[1020,20],"connections":[],"params":["Toma idiota",3]}}
+var brain := {}
 var _start_node
 var _next_node
 var _timer
