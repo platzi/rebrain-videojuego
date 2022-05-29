4b5e467880367c03b16ed4125cf489804c126b0b
arnoldochavez <arnoldochavezlopez@hotmail.com>
2022-05-29 02:15:27 -0600

    Added blinking

--- src/Entity/Area2D.gd
+++ src/Entity/Area2D.gd
@@ -1,11 +1,24 @@
 extends Area2D
 
 
+func _ready() -> void:
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
+
+
 func _input_event(viewport, event, shape_idx):
 	if (
 		event is InputEventMouseButton 
 		and event.button_index == BUTTON_LEFT
 		and event.pressed
+		and Globals.scripting_mode
 	):
-		print(self.get_parent().brain)
-		print(self.get_parent().brain.brain)
+		Globals.open_scripting(get_parent())
+
+
+func _on_mouse_entered() -> void:
+	get_parent().get_node("Sprite").material.set_shader_param("blink", true)
+
+
+func _on_mouse_exited() -> void:
+	get_parent().get_node("Sprite").material.set_shader_param("blink", false)
