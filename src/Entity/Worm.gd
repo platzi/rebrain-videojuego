226ece4aeb42cc0fa8efa63ba3e9c4fa05058dd6
eduardochavez64 <jeduardoochavez@gmail.com>
2022-05-29 03:39:53 -0600

    Delete useless commented old testing code

--- src/Entity/Worm.gd
+++ src/Entity/Worm.gd
@@ -7,14 +7,4 @@ func _ready():
 	brain_dict = {"RotateNode":{"type":"ROTATE","position":[220,40],"connections":[{"from_port":0,"to":"MoveForwardNode","to_port":0}],"params":[1]},"UpdateNode":{"type":"UPDATE","position":[20,60],"connections":[{"from_port":0,"to":"RotateNode","to_port":0}],"params":[]},"MoveForwardNode":{"type":"MOVE_FORWARD","position":[420,40],"connections":[{"from_port":0,"to":"TimerNode","to_port":0}],"params":[2]},"TimerNode":{"type":"TIMER","position":[620,40],"connections":[{"from_port":0,"to":"ShootNode","to_port":0}],"params":[5]},"ShootNode":{"type":"SHOOT","position":[820,60],"connections":[{"from_port":0,"to":"MessageNode","to_port":0}],"params":[]},"MessageNode":{"type":"MESSAGE","position":[1020,20],"connections":[],"params":["Toma idiota",3]}}
 	brain.brain = brain_dict
 	brain.run()
-#func _input(event : InputEvent) -> void:
-#	if event is InputEventKey:
-#		if event.scancode == KEY_ESCAPE and event.pressed:
-#			moving = true
-#			turns_towards("left")
-#		elif event.scancode == KEY_SPACE and event.pressed:
-#			moving = true
-#			turns_towards("right")
-#	else:
-#		moving = false
-#
+
