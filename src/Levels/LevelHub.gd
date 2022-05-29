a973b2af7ac47a52f3ed19e74917ef0f7573356d
arnoldochavez <arnoldochavezlopez@hotmail.com>
2022-05-29 10:00:45 -0600

    Updated level hub dialogue

--- src/Levels/LevelHub.gd
+++ src/Levels/LevelHub.gd
@@ -2,9 +2,9 @@ extends Node
 
 var dialogue_scene := load("res://src/Dialogue/Dialogue.tscn")
 
+onready var ui_canvas_layer : CanvasLayer = $UI
 onready var trigger_zone_1 : Area2D = $Game/TriggerZones/TriggerZone1
 onready var trigger_zone_2 : Area2D = $Game/TriggerZones/TriggerZone2
-onready var ui_canvas_layer : CanvasLayer = $UI
 onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 func _ready():
@@ -20,7 +20,7 @@ func _on_TriggerZone1_body_entered(body : Node) -> void:
 		var dialogue_inst = dialogue_scene.instance()
 		dialogue_inst.connect("finished", animation_player, "play", ["intro_1"])
 		dialogue_inst.dialogues = [
-			"Oye espera un momento..."
+			"Oye, ¡alto ahí!..."
 		]
 		ui_canvas_layer.add_child(dialogue_inst)
 
@@ -29,19 +29,14 @@ func _on_AnimationPlayer_animation_finished(anim : String) -> void:
 		var dialogue_inst = dialogue_scene.instance()
 		dialogue_inst.connect("finished", animation_player, "play", ["intro_2"])
 		dialogue_inst.dialogues = [
-			"¿Eres nuevo por aquí verdad? ¿Cual es tu nombre?",
-			"Oh, con que Arnoldo Chávez, pues ¡bienvenido al [b]Platziverso[/b]!",
-			"Vamos sigueme, la clase esta por comenzar, el profesor ya debe estar online"
+			"¿Formás parte del grupo BETA verdad?",
+			"¡Que emoción! de verdad que no me lo creo que lo haya conseguido, porcierto ¿cual es tu nombre?",
+			"Oh, así que te llamas %s, menúdo nombre, yo diría que tienes más cara de Jane..." % [Globals.player_name],
+			"Pues, no hay que esperar más, ¡vamos sigueme, te espero en la puerta!",
 		]
 		ui_canvas_layer.add_child(dialogue_inst)
-	elif anim == "intro_2":
+	elif anim == "intro_2" or anim == "intro_3":
 		Globals.disable_inputs = false
-#		var dialogue_inst = dialogue_scene.instance()
-#		dialogue_inst.dialogues = [
-#			"Oh, pero si eres [b]Arnoldo Chávez[/b], te estabamos esperando",
-#			"Adelante sigueme"
-#		]
-#		ui_canvas_layer.add_child(dialogue_inst)
 
 
 func _on_TriggerZone2_body_entered(body : Node) -> void:
@@ -52,7 +47,7 @@ func _on_TriggerZone2_body_entered(body : Node) -> void:
 		dialogue_inst.connect("finished", animation_player, "play", ["intro_3"])
 		dialogue_inst.dialogues = [
 			"¿Apoco no esta padrisimo el módelo 3D del edificio de Platzi?",
-			"Una maravilla, parece tan real...",
-			"Bueno, pero dejemonos de charla y vayamos a clase, anda nos vemos alla"
+			"Una maravilla, todo parece tan real... no puedo creer que haya logrado vivir para ver esto",
+			"Bueno, basta ya con la charla y entremos a clase, anda nos vemos alla"
 		]
 		ui_canvas_layer.add_child(dialogue_inst)
