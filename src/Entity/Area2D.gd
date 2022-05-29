2dbe5856861b3b39485b6df36d70494f77779288
eduardochavez64 <jeduardoochavez@gmail.com>
2022-05-28 22:17:37 -0600

    Fixed click function

--- src/Entity/Area2D.gd
+++ src/Entity/Area2D.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 
-func _input(event):
-	if event is InputEventMouseButton:
-		#left: 1, right: 2
-		if event.button_index == BUTTON_LEFT:
-			print(self.get_parent().brain)
-			print(self.get_parent().brain.brain)
+func _input_event(viewport, event, shape_idx):
+	if (
+		event is InputEventMouseButton 
+		and event.button_index == BUTTON_LEFT
+		and event.pressed
+	):
+		print(self.get_parent().brain)
+		print(self.get_parent().brain.brain)
