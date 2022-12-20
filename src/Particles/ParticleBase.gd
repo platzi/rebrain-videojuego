bbdbb2ffd1e2fe3555d723fd322e4912c37723f3
arnoldochavez <arnoldochavezlopez@hotmail.com>
2022-12-19 23:16:29 -0700

    Fixed particles timer

--- src/Particles/ParticleBase.gd
+++ src/Particles/ParticleBase.gd
@@ -5,6 +5,7 @@ func _ready():
 	emitting = true
 	var timer := Timer.new()
 	timer.connect("timeout", self, "queue_free")
-	timer.start(5)
+	timer.autostart = true
+	timer.wait_time = 5
 	add_child(timer)
 	$AudioStreamPlayer2D.play()
