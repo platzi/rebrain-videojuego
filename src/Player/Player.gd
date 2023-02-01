02d7e76e98834d0d40d56468de59567f1fb353ed
arnoldochavez <arnoldochavezlopez@hotmail.com>
2023-02-01 14:42:02 -0700

    Removed player customization box

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -9,35 +9,32 @@ var life = 3
 var inmunity = false
 var inmunity_time = 1
 var player_name = ""
-var is_customizing = true
 var move_towards_vector := Vector2.ZERO
 var is_moving_towards = false
 
-onready var camera_attractor : = $CameraAttractor
-onready var audio_stream_player = $AudioStreamPlayer2D
-onready var animation_tree = $AnimationTree
+onready var audio_stream_player := $AudioStreamPlayer2D
+onready var animation_tree := $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 onready var inmunity_timer := $InmunityTimer
 onready var hair_sprite := $HairSprite
-onready var customization := $Customization
+
+
+var hair_style := 0
+var hair_color := 0
+var skin_color := 0
+var shirt_color := 0
+var pants_color := 0
+var shoes_color := 0
 
 
 func _ready() -> void:
 	inmunity_timer.connect("timeout", self, "remove_inmunity")
-	customization.connect("change_hair", self, "change_hair")
-	customization.connect("change_hair_color", self, "change_hair_color")
-	customization.connect("change_skin_color", self, "change_skin_color")
-	customization.connect("change_shirt", self, "change_shirt")
-	customization.connect("change_pants", self, "change_pants")
-	customization.connect("change_shoes", self, "change_shoes")
-	customization.connect("change_player_name", self, "change_player_name")
-	customization.connect("ended_customizing", self, "ended_customizing")
-	change_hair(customization.all_hairs[customization.current_hair])
-	change_hair_color(customization.hair_colors[customization.current_hair_color])
-	change_skin_color(customization.skin_colors[customization.current_skin_color])
-	change_shirt(customization.clothes_colors[customization.current_shirt])
-	change_pants(customization.clothes_colors[customization.current_pants])
-	change_shoes(customization.clothes_colors[customization.current_shoes])
+	change_hair(Customization.HAIR_STYLES[hair_style])
+	change_hair_color(Customization.HAIR_COLORS[hair_color])
+	change_skin_color(Customization.SKIN_COLORS[skin_color])
+	change_shirt(Customization.CLOTHES_COLOR[shirt_color])
+	change_pants(Customization.CLOTHES_COLOR[pants_color])
+	change_shoes(Customization.CLOTHES_COLOR[shoes_color])
 
 
 func _physics_process(delta : float) -> void:
@@ -47,25 +44,22 @@ func _physics_process(delta : float) -> void:
 		input_vector = input_vector.normalized()
 	else:
 		input_vector = Vector2.ZERO
-	if not is_customizing:
-		if position.distance_to(move_towards_vector) <= 5.0:
-			is_moving_towards = false
-		if is_moving_towards:
-			input_vector = position.direction_to(move_towards_vector)
-			input_vector = input_vector.normalized()
-		if input_vector != Vector2.ZERO:
-			velocity += input_vector * speed
-			if velocity.distance_to(Vector2.ZERO) > MAX_SPEED:
-				velocity = velocity.move_toward(input_vector * MAX_SPEED, delta * MAX_SPEED * 100)
-			animation_tree.set("parameters/Idle/blend_position", input_vector)
-			animation_tree.set("parameters/Move/blend_position", input_vector)
-			animation_state.travel("Move")
-		else:
-			velocity = velocity.move_toward(Vector2.ZERO, delta * MAX_SPEED * 100)
-			animation_state.travel("Idle")
-		velocity = move_and_slide(velocity)
+	if position.distance_to(move_towards_vector) <= 5.0:
+		is_moving_towards = false
+	if is_moving_towards:
+		input_vector = position.direction_to(move_towards_vector)
+		input_vector = input_vector.normalized()
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * speed
+		if velocity.distance_to(Vector2.ZERO) > MAX_SPEED:
+			velocity = velocity.move_toward(input_vector * MAX_SPEED, delta * MAX_SPEED * 100)
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Move/blend_position", input_vector)
+		animation_state.travel("Move")
 	else:
-		camera_attractor.position = customization.position
+		velocity = velocity.move_toward(Vector2.ZERO, delta * MAX_SPEED * 100)
+		animation_state.travel("Idle")
+	velocity = move_and_slide(velocity)
 
 
 func remove_inmunity() -> void:
@@ -113,12 +107,6 @@ func change_shoes(color : Color) -> void:
 
 func change_player_name(_name : String) -> void:
 	player_name = _name
-
-
-func ended_customizing() -> void:
-	is_customizing = false
-	camera_attractor.monitorable = false
-	camera_attractor.monitoring = false
 
 
 func move_towards(position : Vector2) -> void:
